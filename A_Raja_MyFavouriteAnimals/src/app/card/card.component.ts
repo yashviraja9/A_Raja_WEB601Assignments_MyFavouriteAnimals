7b2e60aae1cf53d073d487c70d18567cc4e56ad1
yashviraja <YR6@myscc.ca>
2022-01-26 22:41:45 -0500

    Update card.component.ts

--- A_Raja_MyFavouriteAnimals/src/app/card/card.component.ts
+++ A_Raja_MyFavouriteAnimals/src/app/card/card.component.ts
@@ -8,6 +8,7 @@ import { Animals } from 'src/app/helper-files/content-interface';
 })
 export class CardComponent implements OnInit {
   dog: Animals;
+  tiger: Animals;
 
   constructor() { 
     this.dog = {
@@ -18,6 +19,16 @@ export class CardComponent implements OnInit {
       imgURL: "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
       type: "mammals"
     };
+
+    this.tiger = {
+      id: 2,
+      title: "Tiger",
+      description: "The tiger is the largest living cat species and a member of the genus Panthera",
+      creator: "God",
+      imgURL: "https://cdn.britannica.com/40/75640-050-F894DD85/tiger-Siberian.jpg",
+      type: "Mammalia"
+    };
+
   }
 
   ngOnInit(): void {
