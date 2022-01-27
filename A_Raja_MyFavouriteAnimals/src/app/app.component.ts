4ba48d908e859061854fd211ad4260e05d9d4b03
yashviraja <YR6@myscc.ca>
2022-01-27 11:57:47 -0500

    add <app-card> to app.component.html

--- A_Raja_MyFavouriteAnimals/src/app/app.component.ts
+++ A_Raja_MyFavouriteAnimals/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Animals } from 'src/app/helper-files/content-interface';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
