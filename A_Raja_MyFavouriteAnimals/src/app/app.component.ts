2379fddf2a8ccccd1f4a23dea69c9ed4d2489793
yashviraja <YR6@myscc.ca>
2022-01-27 14:44:42 -0500

    add another records

--- A_Raja_MyFavouriteAnimals/src/app/app.component.ts
+++ A_Raja_MyFavouriteAnimals/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Animals } from 'src/app/helper-files/content-interface';
 
 @Component({
   selector: 'app-root',
--- A_Raja_MyFavouriteAnimals/src/app/helper-files/content-list.ts
+++ A_Raja_MyFavouriteAnimals/src/app/helper-files/content-list.ts
@@ -1,23 +1,42 @@
+//import { ReturnStatement } from '@angular/compiler';
 import { Animals } from 'src/app/helper-files/content-interface';
 
-class AnimalsList {
-    static animalCount = 0;
-    private _animals: Animals[];
+export class AnimalsList {
+    // static animalCount = 0;
+     _animals: Animals[];
 
-    constructor(animal: Animals) {
+    constructor() {
         this._animals = []; //initial array
-        this._animals[0] = animal;
-        this.increaseCount();
+        //this._animals[0] = animal;
+        // this.increaseCount();
     }
 
     get animals(): Animals[] {
         return this._animals;
     }
-    set animals(newAnimals: Animals[]) {
-        this._animals = newAnimals;
+
+    addAnimals(newAnimals: Animals) {
+        this.animals.push(newAnimals);
     }
 
-    increaseCount() {
-        return ++AnimalsList.animalCount;
+    get lengthOfAnimals() {
+        return this._animals.length;
+      }
+    
+    // increaseCount() {
+    //     return ++AnimalsList.animalCount;
+    // }
+    
+    outputAnimals(id:number): string {
+        // return '<div> Title: ' + this.animals[id].title + '<h1></br>',
+        // 'Description: ' + this.animals[id].description + '<h1></br></div>';
+
+        return `<div>
+                <h1>Title: ${this.animals[id].title}</h1></br>
+                <p><b>Description:</b> ${this.animals[id].description}</p></br>
+                <p>Creator: ${this.animals[id].creator}</p></br>
+                <img src="${this.animals[id].imgURL}" width="500" height="350"/>
+                <p>Type: ${this.animals[id].type}</p></br>
+            </div>`;
     }
 }
