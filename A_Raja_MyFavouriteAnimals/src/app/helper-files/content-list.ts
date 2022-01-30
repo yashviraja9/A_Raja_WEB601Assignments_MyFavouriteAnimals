2e03fd0b22ea0a2c30698d065754966bc2969cc9
yashviraja <YR6@myscc.ca>
2022-01-29 20:15:32 -0500

    add error meassage

--- A_Raja_MyFavouriteAnimals/src/app/helper-files/content-list.ts
+++ A_Raja_MyFavouriteAnimals/src/app/helper-files/content-list.ts
@@ -20,12 +20,17 @@ export class AnimalsList {
     }
     
     outputAnimals(id:number): string {
-        return `<div>
+        if(id >= this.lengthOfAnimals()){
+            return 'No records found at index ' +id +'..!!';
+        }
+        else {
+            return `<div>
                 <h2>Title: ${this.animals[id].title}</h2></br>
                 <p><b>Description:</b> ${this.animals[id].description}</p></br>
                 <p><b>Creator:</b> ${this.animals[id].creator}</p></br>
                 <img src="${this.animals[id].imgURL}" width="500" height="350"/>
                 <p><b>Type:</b> ${this.animals[id].type}</p></br>
             </div>`;
+        }
     }
 }
