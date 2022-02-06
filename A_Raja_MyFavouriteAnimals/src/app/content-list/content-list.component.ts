854ec038518868932eeddbb89bbfcfde0e2b3413
yashviraja <YR6@myscc.ca>
2022-02-05 19:51:32 -0500

    change layout 0f Assignment 2

--- A_Raja_MyFavouriteAnimals/src/app/content-list/content-list.component.ts
+++ A_Raja_MyFavouriteAnimals/src/app/content-list/content-list.component.ts
@@ -19,7 +19,7 @@ export class ContentListComponent implements OnInit {
         description: "Dogs are domesticated mammals, not natural wild animals. They were originally bred from wolves. They have been bred by humans for a long time, and were the first animals ever to be domesticated.",
         creator: "God",
         imgURL: "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
-        type: "mammals"
+        type: "Mammals"
       },
       {
         id: 2,
